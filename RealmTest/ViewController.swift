6b0003222e5d1edd385545b3c76ee01221f3bde3
Choi SeungMyeong <710csm@naver.com>
2020-11-15 19:47:57 +0900

    Initial Commit

---
+++ RealmTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmTest
+//
+//  Created by 최승명 on 2020/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
