0b193a50bbbec052e9119fe082db302e44664cbf
Choi SeungMyeong <710csm@naver.com>
2020-11-16 17:00:09 +0900

    first commit

---
+++ RealmTest/Data.swift
@@ -0,0 +1,52 @@
+//
+//  Data.swift
+//  RealmTest
+//
+//  Created by 최승명 on 2020/11/15.
+//
+
+import Realm
+import Foundation
+
+class Dog: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var age = 0
+}
+class Person: Object {
+    @objc dynamic var name = ""
+    @objc dynamic var picture: Data? = nil // optionals supported
+    let dogs = List<Dog>()
+}
+
+// Use them like regular Swift objects
+let myDog = Dog()
+myDog.name = "Rex"
+myDog.age = 1
+print("name of dog: \(myDog.name)")
+
+// Get the default Realm
+let realm = try! Realm()
+
+// Query Realm for all dogs less than 2 years old
+let puppies = realm.objects(Dog.self).filter("age < 2")
+puppies.count // => 0 because no dogs have been added to the Realm yet
+
+// Persist your data easily
+try! realm.write {
+    realm.add(myDog)
+}
+
+// Queries are updated in realtime
+puppies.count // => 1
+
+// Query and update from any thread
+DispatchQueue(label: "background").async {
+    autoreleasepool {
+        let realm = try! Realm()
+        let theDog = realm.objects(Dog.self).filter("age == 1").first
+        try! realm.write {
+            theDog!.age = 3
+        }
+    }
+}
+Realm Studio
--- RealmTest/ViewController.swift
+++ RealmTest/ViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var stuffTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var stuffPrint: UILabel!
+    @IBOutlet weak var priceePrint: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: Results<ShoppingList>?
+    var realm: Realm?
+    var notificationToken: NotificationToken?
+    
+    //MARK: func method
+    deinit{
+        notificationToken?.invalidate()
+    }
+    
+    func inputData(database: ShoppingList) -> ShoppingList{
+        if let name = stuffTextField.text {
+            database.name = name
+        }
+        
+        if let price = priceTextField.text {
+            database.price = price
+        }
+        return database
+    }
+    
+    //MARK: override func method
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        realm = try? Realm()
+        
+        items = realm?.objects(ShoppingList.self)
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        
+        notificationToken = items?.observe({ (change) in
+            print("change : ", change)
+            self.tableView.reloadData()
+        })
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        notificationToken?.invalidate()
+    }
+    
+    //MARK: Action method
+    @IBAction func add(_ sender: Any) {
+        try! realm?.write{
+            realm?.add(inputData(database: ShoppingList()))
+        }
+    }
+    
+    @IBAction func deleteAction(_ sender: Any) {
+        do{
+            try realm?.write {
+                realm?.delete(items!)
+            }
+        }catch{
+            print("Error")
+        }
+    }
+    
+    @IBAction func read(_ sender: Any) {
+        print(realm?.objects(ShoppingList.self))
+    }
+    
+    @IBAction func update(_ sender: Any) {
+        try! realm?.write{
+            guard let items = items else {
+                return
+            }
+            
+            items.forEach({ (list) in
+                if let name = stuffTextField.text, let price = priceTextField.text {
+                    if list.name == name {
+                        list.name = name
+                        list.price = price
+                    }
+                }
+                stuffPrint.text = list.name
+                priceePrint.text = list.price
+            })
+        }
+    }
+    
 }
 
+class ShoppingList: Object{
+    @objc dynamic var name = ""
+    @objc dynamic var price = ""
+}
